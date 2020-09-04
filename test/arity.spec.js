d32f1b2a4251f789783f98e5dd3200208f966140
Hugo <hugosum@PC3343.local>
2020-09-04 18:29:32 +0800

    :construction: Remove recusion function, as binding is only needed for the parent obj

--- test/arity.spec.js
+++ test/arity.spec.js
@@ -11,31 +11,60 @@ describe('curryMethod()', function () {
   })
 
   it('should curry a nullary method', function () {
+    const testObj = {
+      john: {
+        wick: {
+          check (x) {
+            console.log(this)
+          },
+          is: {
+            me (x) {
+              console.log(this)
+            }
+          }
+        }
+      }
+    }
 
+    testObj.john.wick.check()
+    testObj.john.wick.is.me()
+
+    // const duplicateInit = (arr) => {
+    //   const outputArr = []
+    //
+    //   const recursion = (arr, outputArr, timesToRun = arr.length) => {
+    //     // Base case
+    //     if (timesToRun === 0) return outputArr
+    //
+    //     const slicedArr = R.slice(0, timesToRun)(arr)
+    //     const appendedArr = [...outputArr, slicedArr]
+    //     const remainingTimesToRun = timesToRun - 1
+    //     return recursion(arr, appendedArr, remainingTimesToRun)
+    //   }
+    //
+    //   return recursion(arr, outputArr)
+    // }
+    //
     // const pathLens = R.pipe(
     //   R.split('.'),
+    //   duplicateInit,
     //   R.tap(console.log),
-    //   // R.slice(0, layer),
-    //   R.tap(console.log),
-    //   R.lensPath
+    //   R.map(R.lensPath),
+    //   R.tap(console.log)
+    //   // Recursion like factorial
     // )
-    //
-    // // pathLens('john.wick')
     //
     // const getFn = R.curry(
-    //   (fnPath, el) => R.pipe(
-    //     // R.juxt([pathLens(), pathLens(-1)]),
+    //   (path, obj) => R.pipe(
     //     pathLens,
-    //     R.tap(console.log),
-    //     R.map(R.view),
-    //     R.tap(console.log),
-    //     R.map(R.applyTo(el)),
-    //     R.tap(console.log),
-    //     R.apply(R.bind) // Bind the function's parent to prevent error
-    //   )(fnPath)
+    //     R.map(R.view(R.__, obj)),
+    //     R.tap(console.log)
+    //     // R.map(R.bind(R.__, obj))
+    //     // R.bind(R.__, obj)
+    //   )(path)
     // )
-    //
-    // getFn('john.wick', testObj)
+
+    // getFn('john.wick.is.me', testObj)
 
     // Jacky version
 
@@ -49,13 +78,14 @@ describe('curryMethod()', function () {
     // const getFn = R.curry(
     //   (fnPath, el) => R.pipe(
     //     R.juxt([pathLens(), pathLens(-1)]),
+    //     R.map(R.view),
+    //     R.tap(console.log), // Array
+    //     R.map(R.applyTo(el)),
     //     R.tap(console.log),
-    //     R.map(R.view)
-    //     // R.map(R.applyTo(el)),
-    //     // R.apply(R.bind) // Bind the function's parent to prevent error
+    //     R.apply(R.bind) // Bind the function's parent to prevent error
     //   )(fnPath)
     // )
-
+    //
     // getFn('john.wick.is.me', testObj)
   })
 
@@ -64,38 +94,38 @@ describe('curryMethod()', function () {
   })
 
   it('should curry a binary method', function () {
-    const duplicateInit = (arr) => {
-      const outputArr = []
-
-      const recursion = (arr, outputArr, timesToRun = arr.length) => {
-        // Base case
-        if (timesToRun === 0) return outputArr
-
-        const slicedArr = R.slice(0, timesToRun)(arr)
-        const appendedArr = [...outputArr, slicedArr]
-        const remainingTimesToRun = timesToRun - 1
-        return recursion(arr, appendedArr, remainingTimesToRun)
-      }
-
-      return recursion(arr, outputArr)
-    }
-
-    const testArr = ['hello', 'world', 'foo', 'bar']
-    console.log(
-      duplicateInit(testArr)
-    )
+    // const duplicateInit = (arr) => {
+    //   const outputArr = []
+    //
+    //   const recursion = (arr, outputArr, timesToRun = arr.length) => {
+    //     // Base case
+    //     if (timesToRun === 0) return outputArr
+    //
+    //     const slicedArr = R.slice(0, timesToRun)(arr)
+    //     const appendedArr = [...outputArr, slicedArr]
+    //     const remainingTimesToRun = timesToRun - 1
+    //     return recursion(arr, appendedArr, remainingTimesToRun)
+    //   }
+    //
+    //   return recursion(arr, outputArr)
+    // }
+    //
+    // const testArr = ['hello', 'world', 'foo', 'bar']
+    // console.log(
+    //   duplicateInit(testArr)
+    // )
   })
 
   it('should curry a ternary method', function () {
-    // const testObj = {
-    //   john: {
-    //     wick: {
-    //       is: {
-    //         me: (x) => console.log(this)
-    //       }
-    //     }
-    //   }
-    // }
+    const testObj = {
+      john: {
+        wick: {
+          is: {
+            me: (x) => console.log(this)
+          }
+        }
+      }
+    }
     //
     // curryMethod(1, 'john.wick.is.me')('Hello world')(testObj)
   })
--- utilities/helper.js
+++ utilities/helper.js
@@ -1,46 +1,37 @@
 import * as R from 'ramda'
 
 // const duplicateInit = (arr) => {
-//   const duplicatedArr = []
+//   const outputArr = []
 //
-//   const recursion = (arr, duplicatedArr, timesToRun = arr.length) => {
-//     if (timesToRun === 0) return duplicatedArr
+//   const recursion = (arr, outputArr, timesToRun = arr.length) => {
+//     // Base case
+//     if (timesToRun === 0) return outputArr
 //
+//     const slicedArr = R.slice(0, timesToRun)(arr)
+//     const appendedArr = [...outputArr, slicedArr]
 //     const remainingTimesToRun = timesToRun - 1
-//     return recursion(arr, duplicatedArr, remainingTimesToRun)
+//     return recursion(arr, appendedArr, remainingTimesToRun)
 //   }
 //
-//   return recursion(arr, duplicatedArr)
+//   return recursion(arr, outputArr)
 // }
 
-const duplicateInit = (arr) => {
-  const outputArr = []
-
-  const recursion = (arr, outputArr, timesToRun = arr.length) => {
-    // Base case
-    if (timesToRun === 0) return outputArr
-
-    const slicedArr = R.slice(0, timesToRun)(arr)
-    const appendedArr = [...outputArr, slicedArr]
-    const remainingTimesToRun = timesToRun - 1
-    return recursion(arr, appendedArr, remainingTimesToRun)
-  }
-
-  return recursion(arr, outputArr)
-}
+const getParentObj = R.pipe(
+  // Turn an array into two array with slice(1)
+  // [ ['hello', 'world'], ['hello'] ]
+)
 
 const pathLens = R.pipe(
   R.split('.'),
-  R.tap(console.log),
-  // Recursion like factorial
-  R.lensPath
+  getParentObj,
+  R.map(R.lensPath)
 )
 
 const getFn = R.curry(
   (path, obj) => R.pipe(
     pathLens,
-    R.view(R.__, obj),
-    R.bind(R.__, obj)
+    R.map(R.view(R.__, obj)),
+    R.map(R.bind(R.__, obj))
   )(path)
 )
 
