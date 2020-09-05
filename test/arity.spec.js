41b70eff76cf324f09155242f2d912629c988db1
kghugo <howtostudycantonese@gmail.com>
2020-09-05 21:03:39 +0800

    :white_check_mark: Add test for currying method as nullary and unary function

--- test/arity.spec.js
+++ test/arity.spec.js
@@ -3,78 +3,41 @@ const {
 } = require('../index.js')
 const chai = require('chai')
 const expect = chai.expect
+chai.use(require('chai-dom'))
 const R = require('ramda')
 
-describe('curryMethod()', function () {
-  beforeEach(function () {
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
+const { document } = (new JSDOM('...')).window
 
+describe('curryMethod()', function () {
+  let element
+
+  beforeEach(function (done) {
+    element = document.createElement('p')
+
+    done()
   })
 
   it('should curry a nullary method', function () {
-    const testObj = {
-      john: {
-        wick: {
-          check (x) {
-            console.log(x)
-          },
-          is: {
-            me (x) {
-              console.log(this)
-            }
-          }
-        }
-      }
-    }
-
-    // testObj.john.wick.check()
-    // testObj.john.wick.is.me()
-
-    curryMethod(1, 'john.wick.check')('Hello', testObj)
+    const remove = curryMethod(0, 'remove')
+    document.body.append(element)
+    remove(element)
+    expect(document.body).to.not.contains(element)
   })
 
   it('should curry a unary method', function () {
+    const addClass = curryMethod(1, 'classList.add')
+    addClass('test-class')(element)
 
+    expect(element).to.have.class('test-class')
   })
 
   it('should curry a binary method', function () {
-    // const duplicateInit = (arr) => {
-    //   const outputArr = []
-    //
-    //   const recursion = (arr, outputArr, timesToRun = arr.length) => {
-    //     // Base case
-    //     if (timesToRun === 0) return outputArr
-    //
-    //     const slicedArr = R.slice(0, timesToRun)(arr)
-    //     const appendedArr = [...outputArr, slicedArr]
-    //     const remainingTimesToRun = timesToRun - 1
-    //     return recursion(arr, appendedArr, remainingTimesToRun)
-    //   }
-    //
-    //   return recursion(arr, outputArr)
-    // }
 
-    // const g = R.unless(
-    //   R.isEmpty,
-    //   arr => [arr, ...g(R.init(arr))]
-    // )
-    //
-    // const testArr = ['hello', 'world', 'foo', 'bar']
-    // console.log(
-    //   g(testArr)
-    // )
   })
 
   it('should curry a ternary method', function () {
-    const testObj = {
-      john: {
-        wick: {
-          is: {
-            me: (x) => console.log(this)
-          }
-        }
-      }
-    }
-    //
-    // curryMethod(1, 'john.wick.is.me')('Hello world')(testObj)
+
   })
 })
